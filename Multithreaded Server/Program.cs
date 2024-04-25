9410e55d57ac2f3a2e734662888db7b382dd6f70
Ando <androavagan@gmail.com>
2024-04-25 12:58:19 +0300

    Добавьте файлы проекта.

---
+++ Multithreaded Server/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace MultiThreadServer
+{
+    class ExampleTcpListener
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                int MaxThreadsCount = Environment.ProcessorCount * 4;
+                ThreadPool.SetMaxThreads(MaxThreadsCount, MaxThreadsCount);
+                ThreadPool.SetMinThreads(2, 2);
+
+                int port = 8888;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                int counter = 0;
+                server = new TcpListener(localAddr, port);
+                Console.OutputEncoding = Encoding.GetEncoding(866);
+                Console.WriteLine("Конфигурация многопоточного сервера:");
+                Console.WriteLine("IP-адрес: 127.0.0.1");
+                Console.WriteLine("Порт: " + port.ToString());
+                Console.WriteLine("Потоки: " + MaxThreadsCount.ToString());
+                Console.WriteLine("\nСервер запущен\n");
+
+                server.Start();
+
+                while (true)
+                {
+                    Console.Write("Ожидание соединения... ");
+                    ThreadPool.QueueUserWorkItem(ClientProcessing, server.AcceptTcpClient());
+                    counter++;
+                    Console.WriteLine("Соединение №" + counter.ToString() + "!");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+
+            Console.WriteLine("\nНажмите Enter...");
+            Console.Read();
+        }
+
+        static void ClientProcessing(object client_obj)
+        {
+            byte[] bytes = new byte[256];
+            string data = null;
+            TcpClient client = (TcpClient)client_obj;
+
+            data = null;
+            NetworkStream stream = client.GetStream();
+            int i;
+
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                data = Encoding.ASCII.GetString(bytes, 0, i);
+                data = data.ToUpper();
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                stream.Write(msg, 0, msg.Length);
+            }
+
+            client.Close();
+        }
+    }
+}
---
+++ client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NewClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.GetEncoding(866);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("\nСоединение #" + i.ToString() + "\n");
+                Connect("127.0.0.1", "HelloWorld! #" + i.ToString());
+            }
+            Console.WriteLine("\nНажмите Enter...");
+            Console.Read();
+        }
+
+        static void Connect(string server, string message)
+        {
+            try
+            {
+                
+                Int32 port = 8888;
+                TcpClient client = new TcpClient(server, port);
+
+               
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+              
+                NetworkStream stream = client.GetStream();
+
+              
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Отправлено: " + message);
+
+               
+                data = new Byte[256];
+               
+                String responseData = String.Empty;
+              
+                int bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Получено: " + responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+    }
+}
